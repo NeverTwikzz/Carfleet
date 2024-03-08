145398142af048ca48689167a3d73f1efed422e0
Samuel Mottier <sammot@hotmail.ch>
2024-03-08 14:20:24 +0100

    Adaptation class Person et TestPerson

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,34 +1,55 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Person {
     //region private attributes
     private String _name;
+    private String _firstname;
+    private String _phonenumber;
+    private String _emailaddress;
+    private ArrayList<String> _languages;
     //endregion private attributes
 
     //region public methods
-    public Person(String name, String firstname, String phonenumber, String emailaddress, ArrayList<String> languages)
-    {
-        _name = name;
+    public Person(String name, String firstname, String phonenumber, String emailaddress, ArrayList<String> languages) {
+        this._name = name;
+        this._firstname = firstname;
+        this._phonenumber = phonenumber;
+        this._emailaddress = emailaddress;
+        this._languages = languages;
     }
 
-    public String getName(){
+    public String getName() {
         return _name;
     }
 
-    public String getFirstname(){
-        throw new UnsupportedOperationException("Not yet implemented");
+    public String getFirstname() {
+        return _firstname;
     }
 
-    public String getPhonenumber(){
-        throw new UnsupportedOperationException("Not yet implemented");
+    public String getPhonenumber() {
+        return _phonenumber;
     }
 
-    public String getEmailaddress(){
-        throw new UnsupportedOperationException("Not yet implemented");
+    public String getEmailaddress() {
+        return _emailaddress;
     }
 
-    public ArrayList<String> getLanguages(){
-        throw new UnsupportedOperationException("Not yet implemented");
+    public ArrayList<String> getLanguages() {
+        return _languages;
+    }
+    public void setLanguages(List<String> newLanguages) {
+        // Si la liste des langues actuelle est nulle, initialiser une nouvelle ArrayList
+        if (this._languages == null) {
+            this._languages = new ArrayList<>();
+        }
+
+        // Ajouter chaque nouvelle langue à la liste s'il n'y est pas déjà présent pour éviter les doublons
+        for (String language : newLanguages) {
+            if (!this._languages.contains(language)) {
+                this._languages.add(language);
+            }
+        }
     }
     //endregion public methods
 }
